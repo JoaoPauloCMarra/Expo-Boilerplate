4752f728564e8d1741b4bc2575206914aee29d08
John Marra <joaopaulocmarra@gmail.com>
2024-03-04 14:30:00 -0300

    test fix

--- jest.setup.js
+++ jest.setup.js
@@ -5,3 +5,10 @@ import { setUpTests } from 'react-native-reanimated';
 setUpTests();
 
 global.fetch = jest.fn(() => new Promise((resolve) => resolve()));
+
+jest.mock('expo-font');
+jest.mock('expo-asset');
+
+jest.mock('expo-constants', () => ({
+	appOwnership: 'standalone'
+}));
